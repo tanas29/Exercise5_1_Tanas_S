bedcc98f0e935333b28f353138c1bd3a396f9259
tanas29 <tanastum2929@gmail.com>
2022-02-21 09:06:02 +0700

    Create Exercise5_1.py
    ส่งงานแบบฝึกหัด5-1

---
+++ Exercise5_1.py
@@ -0,0 +1,6 @@
+a = int(input("Number1 : "))
+b = int(input("Number2: "))
+print(a, "+", b, "=", (a+b))
+print(a, "-", b, "=", (a-b))
+print(a, "*", b, "=", (a*b))
+print(a, "/", b, "=", int((a/b)))
